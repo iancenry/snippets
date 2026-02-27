9f044c3b6cbdd7c43c446c6978d5552f3dea251d
Ian <iancenry@gmail.com>
2026-02-27 10:38:31 +0300

    Add database connection handling and update dependencies in go.mod

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
 
 type application struct {
@@ -12,7 +16,11 @@ type application struct {
 	errorLog *log.Logger
 }
 
+
+
 func main() {
+	godotenv.Load()
+	// parses runtime config settings for the app
 	defaultAddr := os.Getenv("SNIPPETBOX_ADDR")
 	if defaultAddr == "" {
 		defaultAddr = "127.0.0.1:4000"
@@ -21,6 +29,16 @@ func main() {
 	addr := flag.String("addr", defaultAddr, "HTTP network address")
 	flag.Parse()
 
+	// establishes a connection pool to the database
+	
+	db, err := openDB(os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	
+	defer db.Close(context.Background())
+
+	// establishes dependencies for handlers - loggers
 	infoLog := log.New(os.Stdout, "\033[32mINFO\t\033[0m", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "\033[31mERROR\t\033[0m", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
 
@@ -29,6 +47,7 @@ func main() {
 		errorLog: errorLog,
 	}
 
+	// runs the http server and listens for requests
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
@@ -37,8 +56,23 @@ func main() {
 
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		errorLog.Fatalf("Server failed to start: %v", err)
 	}
+}
+
+
+func openDB(dsn string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
