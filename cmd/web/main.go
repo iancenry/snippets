ee408c3a26bbda9672f07d6f8fd611a6bcefd3e0
Ian <iancenry@gmail.com>
2026-03-07 16:57:32 +0300

    Configure TLS settings for the HTTP server to enhance security- Ciphersuites for TLS < 1.3

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"html/template"
 	"log"
@@ -75,11 +76,27 @@ func main() {
 	}
 	app.templateCache = templateCache
 
+	// configures TLS settings for the server
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+			
+		},
+	}
+
 	// runs the http server and listens for requests
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:   app.routes(),
+		TLSConfig: tlsConfig,
 	}
 
 
